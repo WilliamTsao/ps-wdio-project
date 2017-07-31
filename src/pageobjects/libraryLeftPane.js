f67205b33fcdb96b4ed95ea969830197958e85a3
Eric Olivas <eric@photoshelter.com>
2017-07-31 14:45:48 -0400

    SAP-188 Changed 'newCollection' to 'createNewCollection'

--- src/pageobjects/libraryLeftPane.js
+++ src/pageobjects/libraryLeftPane.js
@@ -12,36 +12,38 @@ class LibraryLeftPane extends Page {
         return super.isLoaded(uimap.isLoaded)
     }
 
-    newCollection(collectionName, isEmbedded = false, collectionPermission) {
+    createNewCollection(collectionName, isEmbedded = false, collectionPermission) {
+        console.log('Creating new collection: ' + collectionName) 
         browser.click(uimap.newCollection)
         NewCollectionGalleryDialog.newCollectionGallery(collectionName, isEmbedded, collectionPermission)
-    }
-
-    newGallery(galleryName, isEmbedded = false, galleryPermission) {
-        browser.click(uimap.newGallery)
-        NewCollectionGalleryDialog.newCollectionGallery(galleryName, isEmbedded, galleryPermission)
+        var isNewCollectionCreated = this.visibleInLeftpane(collectionName)
+        console.log('isNewCollectionCreated: ' + isNewCollectionCreated)
+        return isNewCollectionCreated
     }
 
     visibleInLeftpane(collectionName) {
+        var isVisibleInLeftPane = true
         browser.refresh()
-        this.isLoaded().should.be.true
-        util.waitForLoadingToComplete()
 
-        console.log('Number of items in LeftPane: ' + browser.$$(uimap.topLevelListItems).length)
-
-        var isVisibleInLeftPane = browser.$$(uimap.topLevelListItems).find(function(ele) {
-            console.log('collectionName: ' + collectionName + ', ' + ele.getText('a'))
-            console.log('Object of Type: ' + typeof ele.getText('a'))
-            return ele.getText('a') === collectionName
-        })
-
-        console.log('isVisibleInLeftPane: ' + isVisibleInLeftPane)
-
-        if (result) {
-            return true
-        } else {
-            return false
+        if (this.isLoaded()) {
+            console.log('Number of items in LeftPane: ' + browser.$$(uimap.topLevelListItems).length)
+            var collection = browser.$$(uimap.topLevelListItems).find(function(ele) {
+                console.log('collectionName: ' + collectionName + ', Actual: ' + ele.getText('a'))
+                console.log('Object of Type: ' + typeof ele.getText('a'))
+                return ele.getText('a') === collectionName
+            })
+            if (!collection) {
+                isVisibleInLeftPane = false
+            }
         }
+        console.log('isVisibleInLeftPane: ' + isVisibleInLeftPane)
+        return isVisibleInLeftPane
+    }
+
+    selectParent(){
+        console.log('Selecting parent - of what? Who knows. This is not clear in the test')
+        console.log('However I imagine this is somewhat magical and can be explained.')
+        return this.selectCollectionOrGalleryByName('parent')
     }
 
     selectCollectionOrGalleryByName(collectionName) {
@@ -61,10 +63,6 @@ class LibraryLeftPane extends Page {
         } else {
             return false
         }
-    }
-
-    selectLibraryRoot() {
-        browser.click(uimap.libraryRoot)
     }
 
 }
