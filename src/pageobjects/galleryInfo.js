a0e74de7e0f9ce357d5a3d4bd22e596a1c92df7b
William Tsao <williamtsao@Williams-MacBook-Pro-2.local>
2017-08-08 15:21:45 -0400

    QA-33 Moved logging from rename to isLoaded, removed redundant isLoaded

--- src/pageobjects/galleryInfo.js
+++ src/pageobjects/galleryInfo.js
@@ -7,7 +7,13 @@ class GalleryInfo extends Page{
 
     isLoaded(galleryName){
         return browser.waitUntil(()=>{
-            return super.isLoaded(uimap.isLoaded) && this.getName() === galleryName
+            let domLoaded = super.isLoaded(uimap.isLoaded)
+            let currentName = this.getName()
+            let nameMatches = currentName === galleryName
+            if(nameMatches){
+                console.log(`Current Name: ${currentName}; Expected Name: ${galleryName}`)
+            }
+            return domLoaded && nameMatches
         }, 5000, 'Gallery Info is not loaded after 5s')
     }
 
@@ -33,24 +39,10 @@ class GalleryInfo extends Page{
     }
 
     rename(originalGalleryName, newGalleryName){
-        let renameSuccessful = true
-        if(this.isLoaded(originalGalleryName)){
-            browser.click(uimap.galleryName.form)
-            browser.setValue(uimap.galleryName.input, newGalleryName)
-            browser.click(uimap.galleryName.checkmark)
-            if(this.isLoaded(newGalleryName)){
-                let currentName = this.getName()
-                console.log(`Current name is: ${currentName}; Expected to be: ${newGalleryName}`)
-                renameSuccessful = currentName === newGalleryName
-            }else{
-                console.log('Gallery Inspector is not loaded after rename')
-                renameSuccessful = false
-            }
-        }else{
-            console.log(`gallery inspector is not loaded for ${originalGalleryName}`)
-            renameSuccessful = false
-        }
-        return renameSuccessful
+        browser.click(uimap.galleryName.form)
+        browser.setValue(uimap.galleryName.input, newGalleryName)
+        browser.click(uimap.galleryName.checkmark)
+        return this.isLoaded(newGalleryName)
     }
 
 }
