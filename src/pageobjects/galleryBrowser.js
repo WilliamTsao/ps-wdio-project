cf21ed60a520ee8152263f89281a78c69c1cbe60
William Tsao <williamtsao@Williams-MBP-2.fios-router.home>
2017-08-28 23:36:19 -0400

    QA-41 Moved the isLoaded check from select*ByName to selectItemByName. Making latter more usable in specs.

--- src/pageobjects/galleryBrowser.js
+++ src/pageobjects/galleryBrowser.js
@@ -8,6 +8,7 @@ class GalleryBrowser extends Page {
     isLoaded() {
         return super.isLoaded(uimap.isLoaded)
     }
+<<<<<<< HEAD
 
     selectItemByName(itemName, itemType){
         // browser.refresh()
@@ -24,29 +25,53 @@ class GalleryBrowser extends Page {
         }else{
             console.log(`A ${itemType} named ${itemName} was not found`)
             found = false
+=======
+    selectItemByName(itemName, itemInspector){
+        // browser.refresh()
+        let found = true
+        if(this.isLoaded()){
+            let itemNames = $$(uimap.itemNames(itemInspector.getType()))
+            console.log(`Number of ${itemInspector.getType()} in Center Pane: ${itemNames.length}`)
+            let item = itemNames.find((ele)=>{
+                console.log(`looking for: ${itemName}; current ele name: ${ele.getText()}`)
+                return ele.getText() === itemName
+            })
+            if(item){
+                console.log(`Selecting ${itemName}...`)
+                item.click()
+            }else{
+                console.log(`A ${itemInspector.getType()} named ${itemName} was not found`)
+                found = false
+            }
+>>>>>>> 9cde989... QA-41 Moved the isLoaded check from select*ByName to selectItemByName. Making latter more usable in specs.
         }
+        return found && itemInspector.isLoaded(itemName)
+    }
 
-        return found
+    restoreMediaName(currentInspector, originalName, newName){
+        browser.refresh()
+        this.selectItemByName(newName, currentInspector).should.be.true
+        currentInspector.rename(originalName).should.be.true
     }
 
     selectImageByName(imgName){
         let imageInfo = require('./imageInfo')
-        return this.selectItemByName(imgName, 'IMAGE') && imageInfo.isLoaded(imgName)
+        return this.selectItemByName(imgName, imageInfo)
     }
 
     selectVideoByName(videoName){
         let videoInfo = require('./videoInfo')
-        return this.selectItemByName(videoName, 'VIDEO') && videoInfo.isLoaded(videoName)
+        return this.selectItemByName(videoName, videoInfo)
     }
 
     selectAudioByName(audioName){
         let audioInfo = require('./audioInfo')
-        return this.selectItemByName(audioName, 'AUDIO') && audioInfo.isLoaded(audioName)
+        return this.selectItemByName(audioName, audioInfo)
     }
 
     selectFileByName(docName){
         let fileInfo = require('./fileInfo')
-        return this.selectItemByName(docName, 'DOC') && fileInfo.isLoaded(docName)
+        return this.selectItemByName(docName, fileInfo)
     }
 }
 
